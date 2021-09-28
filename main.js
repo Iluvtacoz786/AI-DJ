8752966514dbc1e0a61af7ff37ebe6da0be56f57
Zoya Rana <74219818+Iluvtacoz786@users.noreply.github.com>
2021-09-27 18:05:15 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-function preload(){}
+var song=""
+function preload(){
+song=loadSound("music.mp3")
+}
 function setup(){
     canvas=createCanvas(600,500);
-    canvas.position(350,150)
+    canvas.position(350,150);
+    video=createCapture(VIDEO);
+    video.hide();
 }
-function draw(){}
+function draw(){
+    image(video,0,0,600,500)
+}
 
 
 
@@ -12,5 +19,9 @@ function draw(){}
 
 
 
-
-function play(){}
+function stop(){
+    song.stop();
+}
+function play(){
+    song.play();
+}
