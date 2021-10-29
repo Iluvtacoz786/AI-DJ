13e4000a788ece1b871c8bb2c502cc87265e07dd
Zoya Rana <74219818+Iluvtacoz786@users.noreply.github.com>
2021-10-28 17:44:16 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var leftWristX=""
 var leftWristY=""
 var rightWristX=""
 var rightWristY=""
+var rightWrist_score=""
+var leftWrist_score=""
 function preload(){
 song=loadSound("music.mp3")
 }
@@ -22,11 +24,36 @@ function draw(){
     fill("red")
     circle(leftWristX,leftWristY,30)
     circle(rightWristX,rightWristY,30)
-    number_leftWristY=Number(leftWristY)
-    round_leftWristY=floor(number_leftWristY);
-    volume=round_leftWristY/500;
-    document.getElementById("volume").innerHTML=volume;
-    song.setVolume(volume);
+    if(leftWrist_score >0.2){
+        number_leftWristY=Number(leftWristY)
+        round_leftWristY=floor(number_leftWristY);
+        volume=round_leftWristY/500;
+        document.getElementById("volume").innerHTML=volume;
+        song.setVolume(volume);
+    }
+    if(rightWrist_score >0.2){
+        if(rightWristY >0 && rightWristY<=100){
+            song.rate(.5)
+            document.getElementById("speed").innerHTML=".5x"
+        }
+        else if(rightWristY>100 && rightWristY<=200){
+            song.rate(1)
+            document.getElementById("speed").innerHTML="1x"
+        }
+        else if(rightWristY>200 && rightWristY<=300){
+            song.rate(1.5)
+            document.getElementById("speed").innerHTML="1.5x"
+        }
+        else if(rightWristY>300 && rightWristY<=400){
+            song.rate(2)
+            document.getElementById("speed").innerHTML="2x"
+        }
+        else if(rightWristY>40000 && rightWristY<=500){
+            song.rate(2.5)
+            document.getElementById("speed").innerHTML="2.5x"
+        }
+    }
+   
 }
 
 function stop(){
@@ -49,5 +76,8 @@ function gotPoses(result){
         console.log(leftWristY);
         console.log(rightWristX);
         console.log(rightWristY);
+        leftWrist_score=result[0].pose.keypoints[9].score;
+        rightWrist_score=result[0].pose.keypoints[10].score;
     }
+    
 }
